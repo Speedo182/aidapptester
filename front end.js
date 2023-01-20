c8d6d1c3cf20a7188650c3c6844df7c725d6a5bd
Speedo182 <123043158+Speedo182@users.noreply.github.com>
2023-01-20 16:16:46 +1100

    Update front end.js

--- front end.js
+++ front end.js
@@ -3,8 +3,54 @@ const codeForm = document.getElementById("code-form");
 const codeInput = document.getElementById("code-input");
 const testCodeButton = document.getElementById("test-code");
 
+// function to save the code
+function saveCode(code) {
+    // make an HTTP request to the backend server to save the code
+    fetch("http://localhost:5000/save-code", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ code: code })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Failed to save code.");
+        }
+    })
+    .catch(error => {
+        console.error(error);
+        alert("Failed to save code. Please try again.");
+    });
+}
+
+// function to deploy and test the code
+function deployTest() {
+    // make an HTTP request to the backend server to deploy and test the code
+    fetch("http://localhost:5000/deploy-test")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Failed to deploy and test code.");
+        }
+        return response.text();
+    })
+    .then(result => {
+        alert(result);
+    })
+    .catch(error => {
+        console.error(error);
+        alert("Failed to deploy and test code. Please try again.");
+    });
+}
+
 codeForm.addEventListener("submit", (event) => {
     event.preventDefault();
+    // validate the form field
+    if (!codeInput.value) {
+        alert("Code input is required!");
+        return;
+    }
+    // save the code
     saveCode(codeInput.value);
     codeInput.value = "";
     testCodeButton.disabled = false;
